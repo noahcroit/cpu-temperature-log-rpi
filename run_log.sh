15842cc039888e0389b2d69797d6dc7788908925
ictlab <kmitl.ac.th>
2023-06-19 05:24:37 +0700

    update

--- run_log.sh
+++ run_log.sh
@@ -3,11 +3,19 @@
 nsamples=100
 period=10
 cnt=1
+
+FILE=log.csv
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists."
+    $(rm $FILE) 
+fi
+
 while [ $cnt -le $nsamples ]
 do
 	temp=$(vcgencmd measure_temp)
 	echo $temp;
-	echo "($period*$cnt),$temp" >> log.csv
+	timestamp=$(($period*$cnt))
+	echo "$timestamp,$temp" >> log.csv
 	((cnt++));
 	sleep $period
 done
